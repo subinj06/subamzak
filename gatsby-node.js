f55fa905cc77ca33b42459b0281035819ee6bf3e
subinj06 <60subin@naver.com>
2026-07-07 16:20:50 +0900

    fix dependencies and configs by codex

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,30 @@
 const path = require('path');
+const webpack = require('webpack');
+
+exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
+  const config = getConfig();
+  const routerHistory = path.resolve(__dirname, 'node_modules/@reach/router/lib/history.js');
+  const routerUtils = path.resolve(__dirname, 'node_modules/@reach/router/lib/utils.js');
+
+  config.resolve = config.resolve || {};
+  config.resolve.alias = {
+    ...(config.resolve.alias || {}),
+    '@reach/router/es/lib/history': routerHistory,
+    '@reach/router/es/lib/history.js': routerHistory,
+    '@reach/router/es/lib/utils': routerUtils,
+    '@reach/router/es/lib/utils.js': routerUtils,
+  };
+
+  config.plugins = config.plugins || [];
+  config.plugins.push(
+    new webpack.NormalModuleReplacementPlugin(
+      /^core-js\/modules\/es\.array\.sort\.js$/,
+      path.resolve(__dirname, 'node_modules/core-js/modules/es.array.sort.js')
+    )
+  );
+
+  actions.replaceWebpackConfig(config);
+};
 
 exports.createPages = ({boundActionCreators, graphql}) => {
   const {createPage} = boundActionCreators;
