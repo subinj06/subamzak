54b6b1053295f25c111dd586282da91e496ff368
subinj06 <60subin@naver.com>
2026-07-08 16:33:47 +0900

    Loading(Static Query) fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
 
 import '../assets/sass/main.scss';
+import config from '../../config';
 import Footer from './Footer';
 import SideBar from './Sidebar';
 
@@ -31,43 +31,28 @@ class Layout extends Component {
     const { children, fullMenu } = this.props;
     const { isPreloaded } = this.state;
     return (
-      <StaticQuery
-        query={graphql`
-          query SiteTitleQuery {
-            site {
-              siteMetadata {
-                title
-              }
-            }
+      <>
+        <Helmet
+          title={config.siteTitle}
+          meta={[
+            { name: 'description', content: 'Spectral' },
+            { name: 'keywords', content: 'site, web' },
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+        <div
+          className={
+            isPreloaded ? 'landing main-body is-preload' : 'landing main-body'
           }
-        `}
-        render={data => (
-          <>
-            <Helmet
-              title={data.site.siteMetadata.title}
-              meta={[
-                { name: 'description', content: 'Spectral' },
-                { name: 'keywords', content: 'site, web' },
-              ]}
-            >
-              <html lang="en" />
-            </Helmet>
-            <div
-              className={
-                isPreloaded
-                  ? 'landing main-body is-preload'
-                  : 'landing main-body'
-              }
-            >
-              <div id="page-wrapper">
-                <SideBar fullMenu={fullMenu} />
-                {children}
-                <Footer />
-              </div>
-            </div>
-          </>
-        )}
-      />
+        >
+          <div id="page-wrapper">
+            <SideBar fullMenu={fullMenu} />
+            {children}
+            <Footer />
+          </div>
+        </div>
+      </>
     );
   }
 }
