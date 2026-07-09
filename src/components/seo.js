c5f0ea7685173135db864696e0dfad3874324f6e
subinj06 <60subin@naver.com>
2026-07-09 10:29:04 +0900

    Remove stale Gatsby build output

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,97 +1,77 @@
 import * as React from "react"
 import Helmet from "react-helmet"
-import {StaticQuery, graphql} from "gatsby"
+import config from "../../config"
 
 
 export const seo = ( { description, keywords, title, url, author,image}) => {
-    return(
-        <StaticQuery
-            query = {detailQuery}
-            reunder = {data => {
-                const metaDescription = description || data.site.siteMetadata.description
-                const metaTitle = title || data.site.siteMetadata.title
-                const metaUrl = url || data.site.siteMetadata.url
-                const metaAuthor = author || data.site.siteMetadata.author
-                const metaImage = image || data.site.siteMetadata.image
-                const metaKeywords = keywords || ["gatsby","Projects","Mechanics","Electronics","Computer","Mechatronics"]
-                return (
-                    <Helmet 
-                    title={title}
-                    meta={[
-                        {
-                            name:`description`,
-                            content: metaDescription,
-                        },
-                        {
-                            property:`og:title`,
-                            content:metaTitle,
-                        },
-                        {
-                            property: `og:description`,
-                            content: metaDescription,
-                        },
-                        {
-                            property: `og:url`,
-                            content: metaUrl,
-                        },
-                        {
-                            property: `og:author`,
-                            content: metaAuthor,
-                        },
-                        {
-                            property: `og:keywords`,
-                            content: metaKeywords,
-                        },
-                        {
-                            property: `og:type`,
-                            content: `website`,
-                        },
-                        {
-                            name:`twitter:card`,
-                            content:metaImage,
-                        },
-                        {
-                            name:`twitter:title`,
-                            content:metaTitle,
-                        },                    
-                        {
-                            name:`twitter:description`,
-                            content:metaDescription,
-                        },  
-                        {
-                            name:`twitter:creator`,
-                            content:metaAuthor,
-                        },
-                        {
-                            name:`twitter:image`,
-                            content:metaImage,
-                        },
+    const metaDescription = description || "Subin's Nightmares are all here..."
+    const metaTitle = title || config.siteTitle
+    const metaUrl = url || "https://subamzak.netlify.app"
+    const metaAuthor = author || "Subin Jeong"
+    const metaImage = image || ""
+    const metaKeywords = keywords || ["gatsby","Projects","Mechanics","Electronics","Computer","Mechatronics"]
 
-                    ].concat(
-                        metaKeywords && metaKeywords.length>0 ? 
-                        {
-                            name:'keywords',
-                            content:metaKeywords.join(`,`),
-                        }
-                        :[]
-                    )}
-                />
-            )
-        }}
-    />
+    return(
+        <Helmet 
+            title={metaTitle}
+            meta={[
+                {
+                    name:`description`,
+                    content: metaDescription,
+                },
+                {
+                    property:`og:title`,
+                    content:metaTitle,
+                },
+                {
+                    property: `og:description`,
+                    content: metaDescription,
+                },
+                {
+                    property: `og:url`,
+                    content: metaUrl,
+                },
+                {
+                    property: `og:author`,
+                    content: metaAuthor,
+                },
+                {
+                    property: `og:keywords`,
+                    content: metaKeywords,
+                },
+                {
+                    property: `og:type`,
+                    content: `website`,
+                },
+                {
+                    name:`twitter:card`,
+                    content:metaImage,
+                },
+                {
+                    name:`twitter:title`,
+                    content:metaTitle,
+                },                    
+                {
+                    name:`twitter:description`,
+                    content:metaDescription,
+                },  
+                {
+                    name:`twitter:creator`,
+                    content:metaAuthor,
+                },
+                {
+                    name:`twitter:image`,
+                    content:metaImage,
+                },
+
+            ].concat(
+                metaKeywords && metaKeywords.length>0 ? 
+                {
+                    name:'keywords',
+                    content:metaKeywords.join(`,`),
+                }
+                :[]
+            )}
+        />
     )
 }
-
-
-const detailQuery = graphql `
-    query DefaultySEROQuery {
-        site{
-            siteMetadata{
-              title
-              description
-              author
-            }
-        }
-    }
-
-`
